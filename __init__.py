755b8933ddf4e8860178668f2f325e35ac459678
yinziyan1206 <408856732@qq.com>
2021-05-27 14:31:32 +0800

    init

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 from ._publisher import Publisher
+from ._builder import setup
 
 __author__ = 'ziyan.yin'
 __version__ = '1.0.0'
 
 
-__all__ = ['Publisher']
+__all__ = ['Publisher', 'setup']
